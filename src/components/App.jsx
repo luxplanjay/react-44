844245606b022d833fd6cc03a0c8de885cc6c8ee
Alexander Repeta <alexander.repeta@gmail.com>
2022-07-06 20:55:10 +0300

    add examples

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,67 @@
 import { Component } from 'react';
-import { HiAcademicCap, HiCalendar, HiArchive } from 'react-icons/hi';
-// import { Alert } from './Alert';
+import PropTypes from 'prop-types';
 import { Box } from './Box';
+import { GlobalStyle } from './GlobalStyle';
 
-const options = [
-  { label: 'a', icon: HiAcademicCap },
-  { label: 'b', icon: HiCalendar },
-  { label: 'c', icon: HiArchive },
-];
+class Popover extends Component {
+  static propTypes = {
+    onClose: PropTypes.func.isRequired,
+  };
 
-export class App extends Component {
-  state = {
-    a: 1,
-    b: 2,
-    c: 3,
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleClose);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleClose);
+  }
+
+  handleClose = e => {
+    if (e.code === 'Escape') {
+      this.props.onClose();
+    }
   };
 
   render() {
+    return <div>Super popover menu</div>;
+  }
+}
+
+export class App extends Component {
+  state = {
+    isOpen: false,
+    modal: null,
+  };
+
+  togglePopover = () => this.setState(state => ({ isOpen: !state.isOpen }));
+
+  render() {
+    const { isOpen } = this.state;
     return (
-      <Box as="main" px={4}>
-        {options.map(({ label, icon: Icon }) => {
-          return (
-            <button key={label}>
-              <Icon />
-              {label}
+      <>
+        <Box as="main" px={4}>
+          <Box as="header" mb={4}>
+            <button onClick={this.togglePopover}>
+              {isOpen ? 'Close' : 'Open'}
             </button>
-          );
-        })}
-      </Box>
+            {isOpen && <Popover onClose={this.togglePopover} />}
+          </Box>
+
+          <button onClick={() => this.setState({ modal: 'edit' })}>
+            Open first
+          </button>
+          <button onClick={() => this.setState({ modal: 'create' })}>
+            Open second
+          </button>
+          <button onClick={() => this.setState({ modal: 'delete' })}>
+            Open third
+          </button>
+          {this.state.modal === 'edit' && <div>First modal</div>}
+          {this.state.modal === 'create' && <div>Second modal</div>}
+          {this.state.modal === 'delete' && <div>Third modal</div>}
+        </Box>
+        <GlobalStyle />
+      </>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './index.css';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
+  // </React.StrictMode>
 );
