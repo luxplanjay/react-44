0ea527128cac3e6fae745c8d3062ad5e9d8b3d9f
Alexander Repeta <alexander.repeta@gmail.com>
2022-07-13 21:27:47 +0300

    add examples

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,48 @@
-import { Component } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Box } from './Box';
 import { GlobalStyle } from './GlobalStyle';
 
-export class App extends Component {
-  render() {
-    return (
-      <>
-        <Box as="main" px={4}>
-          App
-        </Box>
-        <GlobalStyle />
-      </>
-    );
-  }
-}
+const Gallery = ({ items }) => {
+  return (
+    <div>
+      {items.map(item => (
+        <GalleryItem key={item.name} item={item} />
+      ))}
+    </div>
+  );
+};
+
+const GalleryItem = ({ item }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  return (
+    <>
+      <div onClick={() => setIsModalOpen(true)}>{item.name}</div>
+      {isModalOpen && (
+        <Modal itemName={item.name} onClose={() => setIsModalOpen(false)} />
+      )}
+    </>
+  );
+};
+
+const Modal = ({ itemName, onClose }) => {
+  return (
+    <div>
+      Modal: {itemName}
+      <br />
+      <button onClick={onClose}>Close</button>
+    </div>
+  );
+};
+
+export const App = () => {
+  const [items, setItems] = useState([{ name: 1 }, { name: 2 }, { name: 3 }]);
+
+  return (
+    <>
+      <Box as="main" px={4}>
+        <Gallery items={items} />
+      </Box>
+      <GlobalStyle />
+    </>
+  );
+};
