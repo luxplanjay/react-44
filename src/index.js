f5233b07263fe0682cb857d9656da0a74378e847
Alexander Repeta <alexander.repeta@gmail.com>
2022-09-09 19:27:18 +0300

    final

--- src/index.js
+++ src/index.js
@@ -2,22 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { ThemeProvider } from 'styled-components';
-import { BrowserRouter } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react';
-import { Provider } from 'react-redux';
-import { store, persistor } from './redux/store';
 import { theme } from './theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <ThemeProvider theme={theme}>
-            <App />
-          </ThemeProvider>
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
--- src/redux/auth/index.js
+++
@@ -1,3 +0,0 @@
-export { default as authOperations } from './auth-operations';
-export { default as authSelectors } from './auth-selectors';
-export { default as authReducer } from './auth-slice';
